60a918e9e55f5f266704891626f57fe9b05e3ea8
Viktor Krepak <viktorkrepak@gmail.com>
2020-06-08 13:00:38 +0300

    finish base functionality and renders for components

---
+++ src/core/DomListener.js
@@ -0,0 +1,36 @@
+import { capitalize } from '@core/utils';
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`);
+    }
+    this.$root = $root;
+    this.listeners = listeners;
+  }
+
+  initDomListeners() {
+    // console.log(this.listeners);
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      // Abstract wrapper over the AddEventListener
+      const name = this.name || '';
+      if (!this[method]) {
+        throw new Error('Method is not implemented in' + name + 'component');
+      }
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
+    });
+  }
+  removeDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+
+      this.$root.off(listener, this[method]);
+    });
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
